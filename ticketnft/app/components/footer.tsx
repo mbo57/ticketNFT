f1e8ef5ec03a271985bd0e293788184301426d89
sgmikami03 <sgmikami03@gmail.com>
2023-08-19 22:17:49 +0900

    [fix]footer画像変更

--- ticketnft/app/components/footer.tsx
+++ ticketnft/app/components/footer.tsx
@@ -2,13 +2,13 @@
 import * as React from "react";
 
 import Image from "next/image";
-import Logo from "../../public/LogoForStaff.png";
+import Logo from "../../public/LogoBlack.png";
 
 export const Footer = () => {
 
     return (
         <footer className="bg-[#333] h-[30px] flex items-center justify-center">
-            <Image src={Logo} alt="mujiqulo" className="h-[24px] w-auto"></Image>
+            <Image src={Logo} alt="mujiqulo" className="h-[20px] w-auto"></Image>
         </footer>
     );
 };
